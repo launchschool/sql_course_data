889d0a9e2ee87cef805098f6b634040c8e749612
superchilled <karl@superchilled.co.uk>
2017-11-24 16:46:15 +0000

    Adding ls_burger sql file to course data repo

---
+++ sql-and-relational-databases/introduction-to-the-course/read-the-launch-school-sql-book/ls_burger.sql
@@ -0,0 +1,13 @@
+CREATE TABLE orders (
+    id integer,
+    customer_name text,
+    burger text,
+    side text,
+    drink text
+);
+
+INSERT INTO orders VALUES (1, 'Todd Perez', 'LS Burger', 'Fries', 'Lemonade');
+INSERT INTO orders VALUES (2, 'Florence Jordan', 'LS Cheeseburger', 'Fries', 'Chocolate Shake');
+INSERT INTO orders VALUES (3, 'Robin Barnes', 'LS Burger', 'Onion Rings', 'Vanilla Shake');
+INSERT INTO orders VALUES (4, 'Joyce Silva', 'LS Double Deluxe Burger', 'Fries', 'Chocolate Shake');
+INSERT INTO orders VALUES (5, 'Joyce Silva', 'LS Chicken Burger', 'Onion Rings', 'Cola');
